44f14ae7b78831540a77184d826848944e799611
anandmicronubes <86884926+anandmicronubes@users.noreply.github.com>
2021-12-15 09:02:57 +0530

    ok

--- samp_code.py
+++ samp_code.py
@@ -1,3 +1,6 @@
+import boto3
+from codeguru_profiler_agent import Profiler
+
 alph = "abcdefghijklmnopqrstuvwxyz"
 
 print(alph[3:15])
@@ -5,6 +8,16 @@ print(alph[3:15:3])
 print(alph[15:3:-3])
 print(alph[::-3])
 
-conn_string = "ghghhjj"
+conn_string = "ghost"
 pwd = "khk"
-keys="kjhkkhkhj"
+keys = "khaki"
+
+
+def start_application():
+    pass
+
+
+if __name__ == "__main__":
+    custom_session = boto3.session.Session(profile_name='dev', region_name='us-east-1')
+    Profiler(profiling_group_name="pyprofiler", aws_session=custom_session).start()
+    start_application()
